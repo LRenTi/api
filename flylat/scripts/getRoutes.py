79cf8fd066180070884dd47abcc2d433dbaea109
LRenTi <LorenzRenti@outlook.com>
2024-09-08 01:56:10 +0200

    update

--- flylat/scripts/getRoutes.py
+++ flylat/scripts/getRoutes.py
@@ -101,7 +101,7 @@ def get_airport_info(icao_code):
     """Fetches airport information using an external API."""
     load_dotenv()
 
-    api_token = os.environ("API_TOKEN")
+    api_token = os.getenv("API_TOKEN")
     url = f"https://airportdb.io/api/v1/airport/{icao_code}?apiToken={api_token}"
 
     try:
